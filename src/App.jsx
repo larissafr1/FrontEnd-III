809dce5f20b295252e4e4da344968fbd1b4fc3c1
LarissaFelipe <larissafelipereis@gmail.com>
2022-11-09 22:03:28 -0300

    checkpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,92 @@
-
-// Aqui você irá escrever as suas funções de Validação, para verificar se o Formulário foi preenchido corretamente
+import { useState } from "react";
+import { Card } from "./Card";
 
 function App() {
-  // Aqui você irá criar os Estados para manipular os Inputs
 
+  const [errorForm, setErrorForm] = useState(false);
+  const [cardName, setCardName] = useState("");
+  const [colorName, setColorName] = useState("");
+  const [allCards, setAllCards] = useState([
+    {
+      name: "White",
+      color: "#FFFFFF",
+    },
+  ]);
+
+  function addColor(event) {
+    event.preventDefault();
+
+    const newCard = {
+      name: cardName,
+      color: colorName,
+    };
+
+    console.log(newCard);
+
+    if (cardName === "" || colorName === "") {
+      setErrorForm(true);
+    } else {
+      setErrorForm(false);
+      setAllCards([...allCards, newCard]);
+      setCardName("");
+      setColorName("");
+    }
+  }
+
+  
 
   return (
-    <div className="App">
-     <h1>Carga de estudiantes</h1>
-     <form>
-      {/* Comece a desenvolver o seu Código por aqui :) */}
-     </form>
+    <section className="position-relative py-4 py-xl-5 ">
+    <div className="container">
+        <div className="col-md-6 col-xl-4">
+            <div className="card-body d-flex flex-column align-items-center">
+              <div className="App">
+                <div className="title">
+                  <h1>Adicionar nova cor</h1>
+                </div>
+
+                <form
+                  className={errorForm ? "form-error" : ""}
+                  onSubmit={(event) => addColor(event)}
+                >
+                  <div>
+                    <label htmlFor="cardName">Nome</label>
+                    <input
+                      id="cardName"
+                      type="text"
+                      value={cardName}
+                      onChange={(event) => setCardName(event.target.value)}
+                    />
+                  </div>
+
+                  <div>
+                    <label htmlFor="colorName">Código</label>
+                    <input
+                      id="colorName"
+                      type="color"
+                      value={colorName}
+                      onChange={(event) => setColorName(event.target.value)}
+                    />
+                  </div>
+
+                  <button type="submit">Adicionar</button>
+                </form>
+
+                {errorForm ? (
+                  <span>Erro! Por favor, verifique os dados inseridos no formulário</span>
+                ) : null}
+
+                <div className="cards">
+                  {allCards.map((card) => {
+                    return <Card cardData={card} />;
+                  })}
+                </div>
+              </div>
+            </div>
+        </div>
     </div>
-  )
+  </section>
+);
 }
 
 export default App
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,10 @@
-//Este componente deverá receber dados por Props e mostrar as Informações em Tela
-
-export function Card () {
+export function Card (props) {
   return (
-    <div>
-
+    <div className="card-component">
+       <div className="card-body" style={{backgroundColor:props.cardData.color}}>
+          <h3>{props.cardData.name}</h3>
+          <h3>{props.cardData.color}</h3>
+        </div> 
     </div>
   )
 }
